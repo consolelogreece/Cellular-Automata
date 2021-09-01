03e02d34fa93ff1b597eaeb39b873213a578c695
consolelogreece <consolelogreece@gmail.com>
2021-09-01 18:24:23 +0100

    created first iteration - game of life

--- main.cpp
+++ main.cpp
@@ -1,36 +1,76 @@
+#define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
 class CellularAutomata : public olc::PixelGameEngine
 {
 	public:
-		bool* currentState;
-		bool* nextState;
-
 		CellularAutomata()
 		{
 			// Name your application
-			sAppName = "Example";
+			sAppName = "Cellular Automata";
 		}
+	public:
+		bool** currentState;
+		bool** nextState;
+		int w = 1920;
+		int h = 1080;
 
 		bool OnUserCreate() override
 		{
-			nextState = new bool[100, 100];
-			currentState = new bool[100, 100];
+			nextState = new bool*[w + 2];
+			currentState = new bool* [w + 2];
+
+			for (int i = 0; i < w + 2; i++) {
+				nextState[i] = new bool[h + 2]{ true };
+				currentState[i] = new bool[h + 2]{ true };
+			}
 
 			// seed, ignore outer boundary as they wont be drawn for optimaztion purposes.
-			for (int x = 1; x < 99; x++)
+			for (int x = 1; x < w + 2; x++)
 			{
-				for (int y = 1; y < 99; y++)
+				for (int y = 1; y < h + 2; y++)
 				{
-					if (rand() % (2) == 10) nextState[x, y] = true;
+					int  r = rand() % (10);
+					if (r == 5) currentState[x][y] = true;
+					else currentState[x][y] = false;
 				}
 			}
 
 			return true;
 		}
-
+	
 		bool OnUserUpdate(float fElapsedTime) override
-		{			
+		{		
+			for (int x = 2; x < w; x++)
+			{
+				for (int y = 2; y < h; y++)
+				{
+					bool alive = currentState[x][y];
+
+					if (alive) Draw(x, y, olc::WHITE);
+					else Draw(x, y, olc::BLACK);
+
+					int count = 0;
+
+					if (currentState[x - 1][y - 1]) count++;
+					if (currentState[x][y - 1]) count++;
+					if (currentState[x + 1][y - 1]) count++;
+					if (currentState[x - 1][y]) count++;
+					if (currentState[x + 1][y]) count++;
+					if (currentState[x - 1][y + 1]) count++;
+					if (currentState[x][y + 1]) count++;
+					if (currentState[x + 1][y + 1]) count++;
+
+					if (alive && (count < 2 || count > 3)) nextState[x][y] = false;
+					else if (count == 3) nextState[x][y] = true;
+					else nextState[x][y] = alive;
+				}
+			}
+
+			bool** plchldr = currentState;
+			currentState = nextState;
+			nextState = plchldr;
+
 			return true;
 		}
 };
@@ -38,7 +78,7 @@ class CellularAutomata : public olc::PixelGameEngine
 int main()
 {
 	CellularAutomata demo;
-	if (demo.Construct(256, 240, 4, 4))
+	if (demo.Construct(1920, 1080, 1, 1, true))
 		demo.Start();
 	return 0;
 }
